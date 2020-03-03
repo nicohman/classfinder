a9f76c18f140d04a0117cf945883e55a414605f8
Nico Hickman <hickman@wwu.edu>
2020-03-02 16:35:26 -0800

    Add in basic form outline

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,30 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-    </v-app-bar>
-
+    <v-app-bar app color="primary" dark></v-app-bar>
     <v-content>
       <v-container class="fill-height" fluid>
         <v-row align="center" justify="center">
           <v-col class="text-center">
-            <div>
-
-            </div>
+            <v-form ref="form">
+              <v-row align="center" justify="center">
+                <v-col cols="12" sm="3">
+                </v-col>
+                <v-col cols="12" sm="7">
+                </v-col>
+              </v-row>
+              <v-row justify="center">
+                <v-col cols="12" sm="10">
+                </v-col>
+              </v-row>
+              <v-row justify="center">
+                <v-col cols="12" sm="10">
+                </v-col>
+              </v-row>
+              <v-row justify="center">
+                <v-col cols="12" sm="10">
+                </v-col>
+              </v-row>
+            </v-form>
           </v-col>
         </v-row>
       </v-container>
@@ -22,12 +33,10 @@
 </template>
 
 <script>
-
 export default {
   name: 'App',
 
-  components: {
-  },
+  components: {},
 
   data: () => ({
     //

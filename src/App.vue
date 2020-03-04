e940efc8f7da9a51478ae2bc66028596d7928dc0
Nico Hickman <hickman@wwu.edu>
2020-03-03 16:20:58 -0800

    Add instructors and days

--- src/App.vue
+++ src/App.vue
@@ -38,10 +38,27 @@
                 </v-col>
               </v-row>
               <v-row justify="center">
-                <v-col cols="12" sm="10"></v-col>
+                <v-col cols="12" sm="10">
+                  <v-select
+                    v-model="selected.instructor"
+                    :items="options.instructors"
+                    label="Instructors"
+                    item-text="value"
+                    item-value="code"
+                  ></v-select>
+                </v-col>
               </v-row>
               <v-row justify="center">
-                <v-col cols="12" sm="10"></v-col>
+                <v-col cols="12" sm="10">
+                  <v-select
+                    v-model="selected.days"
+                    :items="options.days"
+                    label="Days"
+                    item-text="value"
+                    item-value="code"
+                    multiple
+                  ></v-select>
+                </v-col>
               </v-row>
             </v-form>
           </v-col>
@@ -52,59 +69,80 @@
 </template>
 
 <script>
+// const axios = require('axios');
+
 export default {
   name: 'App',
 
   components: {},
 
-  data: () => ({
-    options: {
-      terms: [{ code: '202020', value: 'Spring 2020' }],
-      gurs: [
-        { code: 'All', value: 'All GUR/Non-GUR Courses' },
-        { code: 'ACOM', value: 'ACOM - A-Communication GUR' },
-        { code: 'ACGM', value: 'ACGM - A-Comparative Gender GUR' },
-        { code: 'BCOM', value: 'BCOM - B-Communication GUR' },
-        { code: 'BCGM', value: 'BCGM - B-Comparative Gender GUR' },
-        { code: 'CCOM', value: 'CCOM - C-Communication GUR' },
-        { code: 'HUM', value: 'HUM - Humanities GUR' },
-        { code: 'LSCI', value: 'LSCI - Nat Sci with Lab Component GUR' },
-        { code: 'SCI', value: 'SCI - Natural Science GUR' },
-        { code: 'QSR', value: 'QSR - Quantitative & Symbolic Reasoning GUR' },
-        { code: 'SSC', value: 'SSC - Social Sciences GUR' },
-      ],
-      others: [
-        { code: 'All', value: 'All Courses With/Without Other Attributes' },
-        { code: 'AFME', value: 'AFME - HistFieldReq-Africa & Mid East' },
-        { code: 'ANCT', value: 'ANCT - HistFieldReq-Ancient' },
-        { code: 'ASIA', value: 'ASIA - HistFieldReq-East & South Asia' },
-        { code: 'CF', value: 'CF - Communication Focus' },
-        { code: 'CF-E', value: 'CF-E - Communication Focus-Economics' },
-        { code: 'CMB1', value: 'CMB1 - HistFieldReq-CombReq600-1450CE' },
-        { code: 'CMB2', value: 'CMB2 - HistFieldReq-CombReqSince1450' },
-        { code: 'CPST', value: 'CPST - Capstone Course' },
-        { code: 'EUR', value: 'EUR - HistFieldReq-Europe' },
-        { code: 'EUR1', value: 'EUR1 - HistFieldReq-Europe 600-1450CE' },
-        { code: 'EUR2', value: 'EUR2 - HistFieldReq-EuropeSince1450CE' },
-        { code: 'FIG', value: 'FIG - FIG First Yr Interest Seminar' },
-        { code: 'FYE', value: 'FYE - First Year Experience' },
-        { code: 'OL', value: 'OL - Online' },
-        { code: 'SL', value: 'SL - Service Learning' },
-        { code: 'TRVL', value: 'TRVL - Faculty Led Travel' },
-        { code: 'US', value: 'US - HistFieldReq-United States' },
-        { code: 'US1', value: 'US1 - HistFieldReq-US before 1865' },
-        { code: 'US2', value: 'US2 - HistFieldReq-US since 1865' },
-        { code: 'WHEM', value: 'WHEM - HistFieldReq-Wstrn Hemisphere' },
-        { code: 'WP1', value: 'WP1 - Writing Proficiency 1 point' },
-        { code: 'WP2', value: 'WP2 - Writing Proficiency 2 points' },
-        { code: 'WP3', value: 'WP3 - Writing Proficiency 3 points' },
-      ],
-    },
-    selected: {
-      term: '',
-      gurs: '',
-      other: '',
-    },
-  }),
+  data: () => {
+    /* let instructors = await axios.get(
+      'classfinder.demenses.net/getInstructors',
+    );
+    instructors = JSON.parse(instructors).map((i) => ({ value: i, code: i })); */
+    const instructors = [{ code: 'h', value: 'h' }];
+    return {
+      options: {
+        terms: [{ code: '202020', value: 'Spring 2020' }],
+        gurs: [
+          { code: 'All', value: 'All GUR/Non-GUR Courses' },
+          { code: 'ACOM', value: 'ACOM - A-Communication GUR' },
+          { code: 'ACGM', value: 'ACGM - A-Comparative Gender GUR' },
+          { code: 'BCOM', value: 'BCOM - B-Communication GUR' },
+          { code: 'BCGM', value: 'BCGM - B-Comparative Gender GUR' },
+          { code: 'CCOM', value: 'CCOM - C-Communication GUR' },
+          { code: 'HUM', value: 'HUM - Humanities GUR' },
+          { code: 'LSCI', value: 'LSCI - Nat Sci with Lab Component GUR' },
+          { code: 'SCI', value: 'SCI - Natural Science GUR' },
+          { code: 'QSR', value: 'QSR - Quantitative & Symbolic Reasoning GUR' },
+          { code: 'SSC', value: 'SSC - Social Sciences GUR' },
+        ],
+        others: [
+          { code: 'All', value: 'All Courses With/Without Other Attributes' },
+          { code: 'AFME', value: 'AFME - HistFieldReq-Africa & Mid East' },
+          { code: 'ANCT', value: 'ANCT - HistFieldReq-Ancient' },
+          { code: 'ASIA', value: 'ASIA - HistFieldReq-East & South Asia' },
+          { code: 'CF', value: 'CF - Communication Focus' },
+          { code: 'CF-E', value: 'CF-E - Communication Focus-Economics' },
+          { code: 'CMB1', value: 'CMB1 - HistFieldReq-CombReq600-1450CE' },
+          { code: 'CMB2', value: 'CMB2 - HistFieldReq-CombReqSince1450' },
+          { code: 'CPST', value: 'CPST - Capstone Course' },
+          { code: 'EUR', value: 'EUR - HistFieldReq-Europe' },
+          { code: 'EUR1', value: 'EUR1 - HistFieldReq-Europe 600-1450CE' },
+          { code: 'EUR2', value: 'EUR2 - HistFieldReq-EuropeSince1450CE' },
+          { code: 'FIG', value: 'FIG - FIG First Yr Interest Seminar' },
+          { code: 'FYE', value: 'FYE - First Year Experience' },
+          { code: 'OL', value: 'OL - Online' },
+          { code: 'SL', value: 'SL - Service Learning' },
+          { code: 'TRVL', value: 'TRVL - Faculty Led Travel' },
+          { code: 'US', value: 'US - HistFieldReq-United States' },
+          { code: 'US1', value: 'US1 - HistFieldReq-US before 1865' },
+          { code: 'US2', value: 'US2 - HistFieldReq-US since 1865' },
+          { code: 'WHEM', value: 'WHEM - HistFieldReq-Wstrn Hemisphere' },
+          { code: 'WP1', value: 'WP1 - Writing Proficiency 1 point' },
+          { code: 'WP2', value: 'WP2 - Writing Proficiency 2 points' },
+          { code: 'WP3', value: 'WP3 - Writing Proficiency 3 points' },
+        ],
+        instructors,
+        days: [
+          { code: 0, value: 'Sunday' },
+          { code: 1, value: 'Monday' },
+          { code: 2, value: 'Tuesday' },
+          { code: 3, value: 'Wednesday' },
+          { code: 4, value: 'Thursday' },
+          { code: 5, value: 'Friday' },
+          { code: 6, value: 'Saturday' },
+        ],
+      },
+
+      selected: {
+        instructor: '',
+        term: '',
+        gurs: '',
+        other: '',
+      },
+    };
+  },
 };
 </script>

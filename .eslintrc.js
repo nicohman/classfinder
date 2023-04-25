0cf16843fa1625a7074b455ee8a1e855de4c5628
Katie Taylor <katiemxtaylor@gmail.com>
2023-04-24 23:40:00 -0700

    update eslint settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,8 +11,10 @@ module.exports = {
     parser: 'babel-eslint',
   },
   rules: {
-  // 'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-  // 'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // 'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'max-len': 'off',
+    'vue/no-unused-components': 'off',
+    'vue/no-unused-variables': 'off',
   }
 };

e0abe76890dccec77c9c62f836df4e74bd9401ed
Katie Taylor <katiemxtaylor@gmail.com>
2023-04-25 01:55:11 -0700

    add term selector btn

--- src/components/scratch-sheet/scratch-sheet.vue
+++ src/components/scratch-sheet/scratch-sheet.vue
@@ -10,11 +10,24 @@
         <v-card justify="center">
           <v-toolbar color="primary" dark flat>
             <v-toolbar-title>Scratch Sheet</v-toolbar-title>
-              <v-spacer />
-              <AllCrnsDialog />
+            <v-spacer class="col-auto" />
+            <!-- <v-spacer /> -->
+            <v-col cols="4">
+              <v-select
+                label="Select a term"
+                hint="Display classes by term"
+                persistent-hint
+                v-model="selectedTerm"
+                :items="terms"
+                solo
+                hide-details
+              />
+            </v-col>
+            <!-- <v-spacer /> -->
+            <AllCrnsDialog/>
           </v-toolbar>
           <v-card-text>
-            <ScratchSheetCalendar />
+            <ScratchSheetCalendar term="selectedTerm" />
           </v-card-text>
         </v-card>
       </v-col>
@@ -27,8 +40,23 @@ import ScratchSheetCalendar from './scratch-sheet-calendar.vue';
 import ScratchSheetList from './scratch-sheet-list.vue';
 import AllCrnsDialog from './all-crns-dialog.vue';
 
+// import { terms } from '../../selectOptions';
+import options from '../../selectOptions';
+
 export default {
   name: 'ScratchSheetPage',
   components: { ScratchSheetCalendar, ScratchSheetList, AllCrnsDialog },
+  data() {
+    return {
+      // selectedTerm: options.terms.slice(-1)[0].value,
+      selectedTerm: '',
+      terms: options.terms.map((termObj) => termObj.value),
+      // terms,
+    };
+  },
+  mounted() {
+    console.log(JSON.stringify(options.terms, null, 2));
+    console.log(JSON.stringify(this.terms, null, 2));
+  },
 };
 </script>
